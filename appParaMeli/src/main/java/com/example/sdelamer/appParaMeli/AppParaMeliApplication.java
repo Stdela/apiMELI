1ef9f0e631a589183122a69e2bcf9fcf3e4b8b1c
santiago delamer <santiago@pop-os.localdomain>
2023-07-11 20:19:33 -0300

    Primer commit

---
+++ appParaMeli/src/main/java/com/example/sdelamer/appParaMeli/AppParaMeliApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdelamer.appParaMeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppParaMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppParaMeliApplication.class, args);
+	}
+
+}
